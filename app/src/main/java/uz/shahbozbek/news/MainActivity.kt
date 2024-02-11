fd11a6060e4b65bd6c3603e7d545880a2e89defa
ShahbozbekCoder <shahbozbey99@gmail.com>
2024-02-11 15:42:07 +0500

    New app

---
+++ app/src/main/java/uz/shahbozbek/news/MainActivity.kt
@@ -0,0 +1,21 @@
+package uz.shahbozbek.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import uz.shahbozbek.news.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomAppBar.setupWithNavController(navHostFragment.navController)
+    }
+}
